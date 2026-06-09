00a820287db11f5ca09cc5ea1d0efbf161d7f2f0
Mahamed Faarah <mohamed.jama@edu.unifi.it>
2026-06-09 11:02:01 +0200

    added testArtworkCreation

--- src/test/java/com/artgallery/model/ArtworkTest.java
+++ src/test/java/com/artgallery/model/ArtworkTest.java
@@ -8,3 +8,11 @@ import org.junit.jupiter.api.DisplayName;
 
 @DisplayName("Artwork Model Tests")
 class ArtworkTest {
+	@Test
+	@DisplayName("Should create artwork with valid parameters")
+	void testArtworkCreation() {
+		Artwork artwork = new Artwork("Starry Night", "Van Gogh", 1000.0);
+		assertThat(artwork.getTitle()).isEqualTo("Starry Night");
+		assertThat(artwork.getArtist()).isEqualTo("Van Gogh");
+		assertThat(artwork.getPrice()).isEqualTo(1000.0);
+	}
