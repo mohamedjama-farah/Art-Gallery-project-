043f1288f7afc55504305e23251751657db38652
Mahamed Faarah <mohamed.jama@edu.unifi.it>
2026-06-09 11:02:08 +0200

    added testMainMethodExists

--- src/test/java/com/artgallery/ArtGalleryAppTest.java
+++ src/test/java/com/artgallery/ArtGalleryAppTest.java
@@ -6,3 +6,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("ArtGalleryApp Tests")
 public class ArtGalleryAppTest {
+    @Test
+    @DisplayName("Should have main method")
+    public void testMainMethodExists() {
+        try {
+            Class<?> appClass = Class.forName("com.artgallery.ArtGalleryApp");
+            assertThat(appClass.getMethod("main", String[].class)).isNotNull();
+        } catch (Exception e) {
+            throw new RuntimeException("ArtGalleryApp.main method not found", e);
+        }
+    }
