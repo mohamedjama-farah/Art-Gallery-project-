fdd285532fc4eb8fe74ff31fdf6d32a8f28355a4
Mahamed Faarah <mohamed.jama@edu.unifi.it>
2026-06-09 11:02:08 +0200

    added ArtGalleryAppTest setup

--- src/test/java/com/artgallery/ArtGalleryAppTest.java
+++ src/test/java/com/artgallery/ArtGalleryAppTest.java
@@ -6,37 +6,3 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("ArtGalleryApp Tests")
 public class ArtGalleryAppTest {
-
-    @Test
-    @DisplayName("Should have main method")
-    public void testMainMethodExists() {
-        try {
-            Class<?> appClass = Class.forName("com.artgallery.ArtGalleryApp");
-            assertThat(appClass.getMethod("main", String[].class)).isNotNull();
-        } catch (Exception e) {
-            throw new RuntimeException("ArtGalleryApp.main method not found", e);
-        }
-    }
-
-    @Test
-    @DisplayName("Should have initializeApp method")
-    public void testInitializeAppMethodExists() {
-        try {
-            Class<?> appClass = Class.forName("com.artgallery.ArtGalleryApp");
-            assertThat(appClass.getDeclaredMethod("initializeApp")).isNotNull();
-        } catch (Exception e) {
-            throw new RuntimeException("ArtGalleryApp.initializeApp method not found", e);
-        }
-    }
-
-    @Test
-    @DisplayName("Application class is properly defined")
-    public void testApplicationClassDefined() {
-        try {
-            Class<?> appClass = ArtGalleryApp.class;
-            assertThat(appClass).isNotNull();
-        } catch (Exception e) {
-            throw new RuntimeException("ArtGalleryApp class error", e);
-        }
-    }
-}
