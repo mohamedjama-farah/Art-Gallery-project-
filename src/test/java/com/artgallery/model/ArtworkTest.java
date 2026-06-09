b7157475bf95f499465d93962b296e85718f1879
Mahamed Faarah <mohamed.jama@edu.unifi.it>
2026-06-09 11:02:02 +0200

    added testHashCode

--- src/test/java/com/artgallery/model/ArtworkTest.java
+++ src/test/java/com/artgallery/model/ArtworkTest.java
@@ -97,3 +97,12 @@ class ArtworkTest {
 		Artwork artwork2 = new Artwork("Starry Night", "Picasso", 1000.0);
 		assertThat(artwork1).isNotEqualTo(artwork2);
 	}
+	@Test
+	@DisplayName("Should handle hashCode correctly")
+	void testHashCode() {
+		Artwork artwork1 = new Artwork("Starry Night", "Van Gogh", 1000.0);
+		artwork1.setYear(1889);
+		Artwork artwork2 = new Artwork("Starry Night", "Van Gogh", 1000.0);
+		artwork2.setYear(1889);
+		assertThat(artwork1.hashCode()).isEqualTo(artwork2.hashCode());
+	}
